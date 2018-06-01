6ef34ab8c95f8122a1358b5973a89f11cb7e38cb
신진명 <sinryang@sinjinmyeong-ui-MacBook-Pro.local>
2018-06-01 14:48:16 +0900

    Remove base.py, add function to import lottery data from url

--- src/datasets/pension_lottery/datasets.py
+++ src/datasets/pension_lottery/datasets.py
@@ -4,19 +4,59 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+import os
 import csv
 import random
+import urllib
 import itertools
+import collections
 
 import numpy as np
 
-import base
-from base import load_pension_lottery_csv
+INPUT_PATH = '../datasets/pension_lottery_data.csv'
+url = 'https://raw.githubusercontent.com/sinryang/random_number_prediction/master/datasets/pension_lottery_data.csv'
 
-INPUT_PATH = '../../../datasets/pension_lottery_data.csv'
+Dataset = collections.namedtuple('Dataset', ['data', 'target'])
+Datasets = collections.namedtuple('Datasets', ['train', 'validation', 'test'])
+
+def load_pension_lottery_data_from_file(filename):
+    with open(filename) as fin:
+        inputs = list(csv.reader(fin))[1:]
+        data = []
+
+        for row in inputs:
+            new_row = []
+
+            for i in range(len(row)):
+                new_row += [float(e)/10 for e in row[i]]
+
+            data.append(new_row)
+
+    return data
+
+def load_pension_lottery_data_from_url(url):
+    fin = urllib.urlopen(url)
+    inputs = list(csv.reader(fin))[1:]
+    data = []
+
+    for row in inputs:
+        new_row = []
+
+        for i in range(len(row)):
+            new_row += [float(e)/10 for e in row[i]]
+
+        data.append(new_row)
+
+    return data
 
 def load_pension_lottery_fcn_datasets(n_data=1, shuffle=False, ratio=[0.9, None, 0.1]):
-    input = load_pension_lottery_csv(INPUT_PATH)
+    if os.path.exists(INPUT_PATH):
+        print('load data from file...')
+        input = load_pension_lottery_data_from_file(INPUT_PATH)
+    else:
+        print('load data from url...')
+        input = load_pension_lottery_data_from_url(url)
+
     n_feature = len(input[0])
 
     data , target = [], []
@@ -37,17 +77,17 @@ def load_pension_lottery_fcn_datasets(n_data=1, shuffle=False, ratio=[0.9, None,
     n_data = len(data)
     n_train = int(n_data * ratio[0])
 
-    train = base.Dataset(data=data[:n_train], target=target[:n_train])
+    train = Dataset(data=data[:n_train], target=target[:n_train])
 
     if ratio[1]:
         n_test = int(n_data * ratio[2])
-        validation = base.Dataset(data=data[n_train:n_data-n_test], target=target[n_train:n_data-n_test])
-        test = base.Dataset(data=data[n_data-n_test:], target=target[n_data-n_test:])
+        validation = Dataset(data=data[n_train:n_data-n_test], target=target[n_train:n_data-n_test])
+        test = Dataset(data=data[n_data-n_test:], target=target[n_data-n_test:])
     else:
         validation = None
-        test = base.Dataset(data=data[n_train:], target=target[n_train:])
+        test = Dataset(data=data[n_train:], target=target[n_train:])
 
-    return base.Datasets(train=train, validation=validation, test=test)
+    return Datasets(train=train, validation=validation, test=test)
 
 if __name__ == '__main__':
     datasets = load_pension_lottery_fcn_datasets(n_data=2, shuffle=True)
